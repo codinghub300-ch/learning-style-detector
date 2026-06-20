fdd8a2558fc0e9734dc33e79f67aaf9776628723
Coding Hub <codinghub300@gmail.com>
2026-06-20 18:54:09 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, jsonify
+from flask import session
 
 app = Flask(__name__)
 
@@ -97,6 +98,8 @@ def index():
 def quiz():
   return render_template("quiz.html", questions=questions)
 
+app.secret_key = "learning-style-secret"
+
 @app.route("/result", methods=["POST"])
 def result():
   answers = request.json
@@ -124,7 +127,62 @@ for dim, values in scores.items():
     else:
         final_result[dim] = "Balanced"
 
-return jsonify(final_result)
+session["result"] = final_result
+
+return {"success": True}
+
+@app.route("/show_result")
+def show_result():
+  result = session.get("result")
+
+games = [
+    {"style": "Active", "url": "https://www.mission-us.org/"},
+    {"style": "Reflective", "url": "https://www.logiclike.com/"},
+    {"style": "Sensing", "url": "https://www.brainpop.com/games/"},
+    {"style": "Intuitive", "url": "https://ncase.me/trust/"},
+    {"style": "Visual", "url": "https://www.crazygames.com/game/color-pixel-art-classic"},
+    {"style": "Verbal", "url": "https://freerice.com/"},
+    {"style": "Sequential", "url": "https://www.coolmathgames.com/0-sudoku"},
+    {"style": "Global", "url": "https://www.sporcle.com/"}
+]
+
+selected_games = []
+
+for style in result.values():
+    for game in games:
+        if game["style"] == style:
+            selected_games.append(game)
+
+return render_template(
+    "result.html",
+    result=result,
+    games=selected_games
+)
+
+# scores = {
+#     "Processing": {"Active": 0, "Reflective": 0},
+#     "Perception": {"Sensing": 0, "Intuitive": 0},
+#     "Input": {"Visual": 0, "Verbal": 0},
+#     "Understanding": {"Sequential": 0, "Global": 0}
+# }
+
+# for i, answer in enumerate(answers):
+#     _, dim, style = questions[i]["options"][answer]
+#     scores[dim][style] += 1
+
+# final_result = {}
+
+# for dim, values in scores.items():
+#     styles = list(values.keys())
+
+#     if values[styles[0]] > values[styles[1]]:
+#         final_result[dim] = styles[0]
+#     elif values[styles[1]] > values[styles[0]]:
+#         final_result[dim] = styles[1]
+#     else:
+#         final_result[dim] = "Balanced"
+
+# return jsonify(final_result)
 
 
 if __name__ == "main":
