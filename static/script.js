48d7b4a088195b35d45f3165151c950762a0fb39
Coding Hub <codinghub300@gmail.com>
2026-06-20 18:50:52 +0300

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,1 +1,35 @@
+document.getElementById("quizForm").addEventListener("submit", async function(e){
+
+e.preventDefault();
+
+let answers=[];
+
+for(let i=0;i<12;i++){
+
+    let answer=document.querySelector(`input[name="q${i}"]:checked`);
+
+    if(!answer){
+        alert("Please answer all questions.");
+        return;
+    }
+
+    answers.push(answer.value);
+}
+
+const response=await fetch("/result",{
+    method:"POST",
+    headers:{
+        "Content-Type":"application/json"
+    },
+    body:JSON.stringify(answers)
+});
+
+const data=await response.json();
+
+localStorage.setItem("result",JSON.stringify(data));
+
+window.location="/show_result";
+
+
+});
 
